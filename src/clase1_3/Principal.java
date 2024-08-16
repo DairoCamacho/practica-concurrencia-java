f27f4a542a7996efbf39473c839a6589d65e6016
DairoCamacho <74800666+DairoCamacho@users.noreply.github.com>
2024-08-16 13:52:07 -0500

    tercera forma de crear un hilo usando una clase anonima interna

---
+++ src/clase1_3/Principal.java
@@ -0,0 +1,48 @@
+package clase1_3;
+
+public class Principal {
+    /**
+     * No facilitar reutilizar código
+     * <p>
+     * Puede ser util cuando es muy complicado pasar la información a la otra tarea
+     * */
+    public static void main(String[] args) {
+        System.out.println("Inicio del hilo principal \n");
+
+        final int numeroVeces = 5;
+
+        Thread hilo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < numeroVeces; i++) {
+                    System.out.println("Soy el hilo: " + Thread.currentThread().getName());
+
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        hilo.start();
+
+        Thread hilo1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < numeroVeces; i++) {
+                    System.out.println("Soy el hilo: " + Thread.currentThread().getName());
+
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        hilo1.start();
+
+        System.out.println("\nFin del hilo principal \n");
+    }
+}
